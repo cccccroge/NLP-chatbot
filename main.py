4895794cf45ccb9afd3ee27fd53f01acdb88b7cf
cccccroge <pooopooo543@gmail.com>
2019-11-14 22:49:42 +0800

    Add UI but need to be fixed

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ kivy.require('1.11.1')
 
 from kivy.app import App
 from kivy.config import Config
-from kivy.uix.label import Label
 from kivy.uix.widget import Widget
-from kivy.uix.layout import Layout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
 from gpt2_pirate import Our_gpt2
 
 
@@ -20,7 +17,30 @@ class NLPBotApp(App):
 
 
 class MainWindow(Widget):
-    pass
+    hint_layout = ObjectProperty(None)
+    functions_layout = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.hideAll()
+
+    def hideAll(self):
+        self.hint_layout.opacity = 0
+        self.hint_layout.disabled = True
+        self.functions_layout.opacity = 0
+        self.functions_layout.disabled = True
+
+    def on_click_bot(self):
+        self.hint_layout.opacity = 1
+        self.hint_layout.disabled = False
+
+    def on_click_hint(self):
+        self.hint_layout.opacity = 0
+        self.hint_layout.disabled = True
+        self.functions_layout.opacity = 1
+        self.functions_layout.disabled = False
+
+
 
 if __name__ == '__main__':
     Config.set('graphics', 'fullsreen', 1)
