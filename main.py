f4c176d5e783b5454ed299b0c953d35b05d22795
cccccroge <pooopooo543@gmail.com>
2019-11-14 19:06:49 +0800

    Add robot rsc

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from kivy.config import Config
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.layout import Layout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from gpt2_pirate import Our_gpt2
 
 
@@ -19,7 +21,6 @@ class NLPBotApp(App):
 
 class MainWindow(Widget):
     pass
-
 
 if __name__ == '__main__':
     Config.set('graphics', 'fullsreen', 1)
