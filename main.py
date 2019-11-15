8675ffdc4332090792e22792723690a70f66795c
cccccroge <pooopooo543@gmail.com>
2019-11-15 23:08:13 +0800

    Add more ui

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ kivy.require('1.11.1')
 from kivy.app import App
 from kivy.config import Config
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, OptionProperty, DictProperty
 from gpt2_pirate import Our_gpt2
+from message import msg
 
 
 class NLPBotApp(App):
@@ -17,28 +18,93 @@ class NLPBotApp(App):
 
 
 class MainWindow(Widget):
-    hint_layout = ObjectProperty(None)
+    output_layout = ObjectProperty(None)
+    output_label = ObjectProperty(None)
     functions_layout = ObjectProperty(None)
+    input_layout = ObjectProperty(None)
+    bottom_layout = ObjectProperty(None)
+
+    state = OptionProperty("NONE", options=["NONE", "WAIT_SERVE", "HINT_SHOWN", "SELECT_FUNC", \
+    "ENTER_INPUT", "WAIT_RESPONSE", "OUTPUT_SHOWN"])
+    message = DictProperty(msg)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.hideAll()
+        self.state = "WAIT_SERVE"
 
     def hideAll(self):
-        self.hint_layout.opacity = 0
-        self.hint_layout.disabled = True
-        self.functions_layout.opacity = 0
-        self.functions_layout.disabled = True
+        self.output_layout.opacity = 0
+        self.output_layout.disabled = True
+
+        self.toggleLayout('input')
+        self.toggleLayout('functions')
+
+
+    def toggleLayout(self, which):
+        if (which == 'output'):
+            if (self.output_layout.disabled):
+                self.output_layout.opacity = 1
+                self.output_layout.disabled = False
+            else:
+                self.output_layout.opacity = 0
+                self.output_layout.disabled = True
+        elif (which == 'input'):
+            if (self.state == 'ENTER_INPUT'):   # show input
+                self.bottom_layout.add_widget(self.input_layout)
+            else:
+                self.bottom_layout.remove_widget(self.input_layout)
+
+        elif (which == 'functions'):
+            if (self.state == 'SELECT_FUNC'):   # show functions
+                self.bottom_layout.add_widget(self.functions_layout)
+            else:
+                self.bottom_layout.remove_widget(self.functions_layout)
+
+    def setRobotText(self, which):
+        self.output_label.text = self.message[which]
 
     def on_click_bot(self):
-        self.hint_layout.opacity = 1
-        self.hint_layout.disabled = False
+        if (self.state == "WAIT_SERVE"):
+            self.state = "HINT_SHOWN"
+            self.toggleLayout('output')
+            
 
     def on_click_hint(self):
-        self.hint_layout.opacity = 0
-        self.hint_layout.disabled = True
-        self.functions_layout.opacity = 1
-        self.functions_layout.disabled = False
+        if (self.state == "HINT_SHOWN"):
+            self.state = "SELECT_FUNC"
+            self.toggleLayout('output')
+            self.toggleLayout('functions')
+    
+
+    def on_click_a(self):
+        if (self.state == "SELECT_FUNC"):
+            self.state = "ENTER_INPUT"
+            self.toggleLayout('functions')
+            self.toggleLayout('input')
+            self.toggleLayout('output')
+            self.setRobotText('func_a_hint')
+
+    def on_click_b(self):
+        if (self.state == "SELECT_FUNC"):
+            self.state = "ENTER_INPUT"
+            self.toggleLayout('functions')
+            self.toggleLayout('input')
+            self.toggleLayout('output')
+            self.setRobotText('func_b_hint')
+
+    def on_click_c(self):
+        if (self.state == "SELECT_FUNC"):
+            self.state = "ENTER_INPUT"
+            self.toggleLayout('functions')
+            self.toggleLayout('input')
+            self.toggleLayout('output')
+            self.setRobotText('func_c_hint')
+            
+
+    # For debug
+    def on_state(self, instance, value):
+        print("state beocme: " + value)
 
 
 
---
+++ message.py
@@ -0,0 +1,12 @@
+msg = {
+    'welcome': 'Hello, welcome back!\nHow can I help you?',
+
+    'func_a': 'Could you tell a story for me?',
+    'func_a_hint': 'Of course I can!\nBut maybe you can start with one or two sentences...',
+
+    'func_b': 'How about guess what I\'m feeling right now?',
+    'func_b_hint': 'Say something then I\'ll try my best to guess.',
+
+    'func_c': 'I want to share with you what happened today...',
+    'func_c_hint': 'Go ahead!'
+}
